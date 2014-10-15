6b20e0b992e5491fe208cc2b4db05a68bbfaaf90
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-16 00:12:40 +0800

    blubird

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var q= require('q'),
+var Promise= require('bluebird'),
   nodemailer = require('nodemailer')
 
 module.exports = {
@@ -9,14 +9,7 @@ module.exports = {
   },
   send : function( mailOptions){
     var root = this
-    return q.Promise(function( resolve, reject){
-//      var mailOptions = {
-//        from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-//        to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-//        subject: 'Hello ✔', // Subject line
-//        text: 'Hello world ✔', // plaintext body
-//        html: '<b>Hello world ✔</b>' // html body
-//      };
+    return new Promise(function( resolve, reject){
 
       root.transporters[root.relier].sendMail(mailOptions, function(error, info){
         if(error){
